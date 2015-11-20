1860a462481106117a35bbac5063da7c5fc9aa89
Timo Stollenwerk <tisto@plone.org>
2015-11-20 16:28:22 +0100

    Look up the Django model and pass it to the JsonSchemaSerializer.

--- rest_framework_schemaform/renderers.py
+++ rest_framework_schemaform/renderers.py
@@ -11,6 +11,13 @@ class JSONSchemaRenderer(renderers.JSONRenderer):
     charset = 'utf-8'
 
     def render(self, data, media_type=None, renderer_context=None):
-        serializer = JsonSchemaSerializer()
+        # Look up the Django model
+        view = renderer_context.get('view')
+        queryset = view.get_queryset()
+        model = queryset.model
+        instance = model()
+        # Pass the Django model to the serializer
+        serializer = JsonSchemaSerializer(instance=instance)
+        # Render the serializer result to json
         result = serializer.to_representation(data)
         return json.dumps(result)
