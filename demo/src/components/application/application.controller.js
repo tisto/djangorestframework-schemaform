c5c3b0526788ebab62afeda3196bbf5dd068f855
Timo Stollenwerk <tisto@plone.org>
2016-02-04 15:35:02 +0100

    Fetch application schema/form from backend.

--- demo/src/components/application/application.controller.js
+++ demo/src/components/application/application.controller.js
@@ -1,39 +1,19 @@
 class ApplicationController {
 
   constructor(applicationService) {
-    this.name = 'Contact Us';
-    this.model = {};
-    this.schema = {
-      type: "object",
-      properties: {
-        name: { type: "string", minLength: 2, title: "Name", description: "Name or alias" },
-        title: {
-          type: "string",
-          enum: ['dr','jr','sir','mrs','mr','NaN','dj']
-        }
-      },
-      "required": [
-        "name",
-      ]
-    };
-    this.form = [
-      "*",
-      {
-        type: "submit",
-        title: "Save"
-      }
-    ];
+    this.name = 'Application';
     this.result = {};
     this.service = applicationService;
+    this.model = {};
+    this.schema = {};
+    this.form = [];
+    this.getSchema();
   }
 
   getSchema() {
     this.service.getSchema().then((res) => {
-      this.result = res.data;
-      console.log(this.result);
-      this.schema = res.data;
-      this.form = res.data.form;
-      alert('getSchema');
+      this.schema = res;
+      this.form = res.form;
     });
   }
 
@@ -43,9 +23,9 @@ class ApplicationController {
 
     // Then we check if the form is valid
     if (form.$valid) {
-      console.log('form is valid')
+      console.log('form is valid');
     } else {
-      console.log('form is invalid')
+      console.log('form is invalid');
     }
   }
 }
